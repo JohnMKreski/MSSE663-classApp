2d107ab1619a93e53ccf98fd67b075374a52a5a4
JohnMKreski <jkreski@regis.edu>
2024-09-04 22:00:38 -0600

    Week 2 Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
+//Components for all of the app
+//add components to the imports that are sharedover the entire app
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Hello World!';
+  title = 'Hello World (app.component.ts)!';
+
+  // @HostListener('window:scroll')
+  // onScroll() {
+  //   console.log("Scrolling")
+  // }
+  // @HostListener('contextmenu')
+  // orRightClick(event: any) {
+  //  // event.preventDefault();
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PizzaAppComponent } from './pizza-app/pizza-app.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+      path: 'home',
+      component: HomeComponent,
+    },
+    {
+      path: 'pizzas',
+      component: PizzaAppComponent,
+    },
+    {
+      path: 'about',
+      component: AboutComponent,
+    },
+  ];
